6c130fbc50428b3a20253f3728ebcfe53e6ca9d4
stenio bomfim <stenio_lima2016@hotmail.com>
2024-06-22 08:47:04 -0300

    Initial Commit

---
+++ listagemdeprodutos/listagemdeprodutosApp.swift
@@ -0,0 +1,17 @@
+//
+//  listagemdeprodutosApp.swift
+//  listagemdeprodutos
+//
+//  Created by stenio Lima on 22/06/24.
+//
+
+import SwiftUI
+
+@main
+struct listagemdeprodutosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
